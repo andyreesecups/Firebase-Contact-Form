c7fa98a567db2ab9f009ff1b3bb7cf879bb35fde
andyreesecups <andyreesecups@yahoo.com>
2017-09-02 10:21:44 -0500

    connected to database, added setTimeout funtionality for validation message

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDIn01WC31r77ovQ0qz1eQHB1feTtnf-QA",
+    authDomain: "contactform-17eb3.firebaseapp.com",
+    databaseURL: "https://contactform-17eb3.firebaseio.com",
+    projectId: "contactform-17eb3",
+    storageBucket: "contactform-17eb3.appspot.com",
+    messagingSenderId: "473954672990"
+  };
+  firebase.initializeApp(config);
+
+  // Reference message collection
+  var messagesRef = firebase.database().ref('messages');
+
 // Listen for form submit
 document.getElementById('contactForm').addEventListener('submit', submitForm);
 
+// Submit form
 function submitForm(e) {
     e.preventDefault();
 
@@ -10,5 +25,31 @@ function submitForm(e) {
     var phone = document.getElementById('phone').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
-    console.log(name);
+    // console.log(name);
+
+	// Save Message
+    saveMessage(name, company, email, phone, message);
+
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // Hide alert after 3 seconds
+    setTimeout(function() {
+    document.querySelector('.alert').style.display = 'none';	
+    }, 3000);
+
+    // Reset form after submissions
+    document.getElementById('contactForm').reset();
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message) {
+	var newMessageRef = messagesRef.push();
+	newMessageRef.set({
+		name: name,
+		company: company,
+		email: email,
+		phone: phone,
+		message: message
+	});
 }
